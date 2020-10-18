6b3b9ba88a01807edb611dd75b65a2a7f6c2f611
Christoph Thurnheer <c.thurnheer@gmx.ch>
2020-10-18 15:36:42 +0200

    added c++20 support for the project
    so cop will compile with zephyr

--- README.md
+++ README.md
@@ -12,4 +12,11 @@ west update
 west build -b nucleo_f030r8 copOnZephyr/app/
 ```
 
+This project only builds with the (currently alpha) toolchain version 0.12.0
+
+For linux on x86 64Bit versions:
+wget https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.12.0-alpha-1/zephyr-sdk-0.12.0-alpha-1-x86_64-linux-setup.run
+chmod +x zephyr-sdk-0.12.0-alpha-1-x86_64-linux-setup.run
+./zephyr-sdk-0.12.0-alpha-1-x86_64-linux-setup.run
+
 
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,73 @@
-#include <cop/Event.hpp>
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <drivers/uart.h>
+#include <cop/TransportLinkLayer.hpp>
+#include <cop/DataLinkLayer.hpp>
+#include <array>
+
+class Adapter {
+public:
+    void send(std::byte& data) noexcept {
+        buffer_[pos_++] = data;
+        if(pos_ == size_)
+            pos_ = 0;
+    }
+private:
+    static constexpr size_t size_ = 512;
+    std::array<std::byte, size_> buffer_;
+    int pos_ = 0;
+};
+
+enum Events {
+    eHelloWorld
+};
+
+struct HelloWorld : cop::Event<eHelloWorld> {
+    int data = 0;
+
+    template<class Coder>
+    auto parse(Coder coder) {
+        return coder | data;
+    }
+};
+
+class Handler {
+public:
+    void handle(HelloWorld& hw) {
+        printk("hw reseived\n");
+    }
+};
+
+class Channel {
+public:
+    Channel(Handler& handler) : buffer_(), adapter_(), tll_(handler) {}
+
+    void sendEvent(HelloWorld& hw) {
+        auto it = buffer_.begin(); auto end = buffer_.end();
+        tll_.sendEvent(hw, it, end);
+        cop::DataLinkLayer<ReadIt> dll_(it, end);
+        dll_.send(adapter_);
+    }
+        
+private:
+    static constexpr size_t size_ = 512;
+    std::array<std::byte, size_> buffer_;
+    using ReadIt = std::array<std::byte, size_>::iterator;
+    Adapter adapter_;
+    cop::TransportLinkLayer<Handler, ReadIt, std::tuple<HelloWorld> >tll_;
+
+};
 
 int main() {
+
+    HelloWorld hw;
+    Handler h;
+    Channel c(h);
+    while(1) {
+        hw.data++;
+        c.sendEvent(hw);
+        k_msleep(1000);
+    }
 
     return 0;
 }
